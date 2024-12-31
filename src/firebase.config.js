e30e9df685e80ba3909a48babb01dfa748e2a227
Aqil321821 <aqilrao321821@gmail.com>
2025-01-01 00:40:50 +0500

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFrirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCIgbRA_5lhMr7OEa8ZsP43KBxW0z9CgZg',
+  authDomain: 'house-app-d1fd6.firebaseapp.com',
+  projectId: 'house-app-d1fd6',
+  storageBucket: 'house-app-d1fd6.firebasestorage.app',
+  messagingSenderId: '401187598577',
+  appId: '1:401187598577:web:d8a7721badf6231751f950',
+};
+
+initializeApp(firebaseConfig);
+export const db = getFrirestore();
